5da81f6a9289557dd3ee5be29424a2aa28f4f62e
Melbourne <jiahe.zhang@ucdconnect.ie>
2022-03-14 11:08:41 +0800

    2022.3.14ZJH time:1

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,10 @@
-from flask_wtf import form
+from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
 from wtforms.validators import DataRequired
 
 
-'''class LoginForm(form):
+class LoginForm(FlaskForm):
     username = StringField('用户名', validators=[DataRequired(message='请输入名户名')])
     password = PasswordField('密码', validators=[DataRequired(message='请输入密码')])
     remember_me = BooleanField('记住我')
-    submit = SubmitField('登录')'''
+    submit = SubmitField('登录')
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,20 +1,23 @@
 from flask import render_template, flash, redirect
 from app.main import app
-'''from app.main.forms import LoginForm'''
+from app.main.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-    return 'Hello World!'
+    user = {'username': 'group1Mel'}
+    posts = [
+
+    ]
+    return render_template('index.html', title='Home', user=user, posts = posts)
 
 
-'''@app.route('/login')
+@app.route('/login')
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
+    '''if form.validate_on_submit():
         flash('用户登录的名户名是:{} , 是否记住我:{}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title='登 录', form=form)'''
+        return redirect('/index')'''
+    return render_template('login.html', title='登 录', form=form)
