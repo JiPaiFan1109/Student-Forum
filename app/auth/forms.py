f965e16a5732e1cf5eaefc4202a587a3345b0dbc
Feihe Huang <feihe.huang@ucdconnect.ie>
2022-03-16 23:03:01 +0800

    bootstrap and user info

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -6,10 +6,10 @@ from ..models import User
 
 
 class LoginForm(Form):
-    email = StringField('用户名', validators=[InputRequired(message='请输入用户名')])
-    password = PasswordField('密码', validators=[InputRequired(message='请输入密码')])
-    remember_me = BooleanField('记住我')
-    submit = SubmitField('登录')
+    text = StringField('text', validators=[InputRequired(message='请输入用户名')])
+    password = PasswordField('password', validators=[InputRequired(message='请输入密码')])
+    remember_me = BooleanField('remember_me')
+    submit = SubmitField('submit')
 
     def validate_on_submit(self):
         pass
