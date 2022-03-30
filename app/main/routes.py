dbf8a9b329222c71a47f981fc9fb5ec39ba8a557
Melbourne <jiahe.zhang@ucdconnect.ie>
2022-03-30 13:03:27 +0800

    2022.3.30ZJH time:2 数据库更新以及pagedown和markdown的配置

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, session
+from flask import render_template, flash, redirect, url_for, session, abort
 from flask_login import login_required, current_user
 from . import main
 from .forms import EditProfileForm, PostForm
@@ -19,12 +19,17 @@ def index():
     return render_template('index.html', form=form, posts=posts)
 
 
+'''同理要和userinfo连起来，这里提供用户的帖子记录posts'''
 @main.route('/user/<username>')
 def user(username):
-    user = User.query.filter_by(usernamne=username).first_or_404()
-    return render_template('user1.html', user=user)
+    user = User.query.filter_by(usernamne=username).first()
+    if user is None:
+        abort(404)
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user1.html', user=user, posts=posts)
 
 
+'''要和userinfo连起来，把对应的数据显示到对应位置，这里提供数据库里对应的数据物体'''
 @main.route('/edit-profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 import datetime
-
+from markdown import markdown
+import bleach
 from app import db
 from flask_login import UserMixin, AnonymousUserMixin
 from . import login_manager
@@ -152,8 +153,21 @@ class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em',
+                        'i', 'li', 'ol', 'pre', 'strong', 'ul', 'h1',
+                        'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags, strip=True))
+
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
 
 
 login_manager.anonymous_user = AnonymousUser
