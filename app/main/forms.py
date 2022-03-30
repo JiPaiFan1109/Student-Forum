b3d2a947fe57504f6ae7cf2bc4511dc95d8cc5b7
Melbourne <jiahe.zhang@ucdconnect.ie>
2022-03-30 13:03:27 +0800

    2022.3.30ZJH time:2 数据库更新以及pagedown和markdown的配置

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import InputRequired, Length
-
+from flask_pagedown.fields import PageDownField
 
 class EditProfileForm(FlaskForm):
     name = StringField('Real name', validators=[Length(0, 64)])
