bb5bcf971b8161d70e992d672a036ed85d0166c9
Melbourne <jiahe.zhang@ucdconnect.ie>
2022-04-05 21:07:48 +0800

    2022.4.05ZJH time:9 我的帖子分页

--- app/main/routes.py
+++ app/main/routes.py
@@ -31,8 +31,13 @@ def user(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         abort(404)
-    posts = user.posts.order_by(Post.timestamp.desc()).all()
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    pagination = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=current_app.config['FLASK_POSTS_PER_PAGE'],
+        error_out=False)
+    posts = pagination.items
+    return render_template('user.html', user=user, posts=posts,
+                           pagination=pagination)
 
 
 '''要和userinfo连起来，把对应的数据显示到对应位置，这里提供数据库里对应的数据物体'''
