a8a34fb964cb1fd22ae4606a0e6fa4f9c698806b
Feihe Huang <feihe.huang@ucdconnect.ie>
2022-04-08 23:16:39 +0800

    HFH 4.8

--- app/main/forms.py
+++ app/main/forms.py
@@ -27,7 +27,7 @@ class EditProfileForm(FlaskForm):
 
 
 class PostForm(FlaskForm):
-    title = TextAreaField('Add a title here', validators=[InputRequired()])
-    body = TextAreaField('Post whatever you want?', validators=[InputRequired()])
+    title = TextAreaField('Change your title here:', validators=[InputRequired()])
+    body = TextAreaField('Change your post here:', validators=[InputRequired()])
     submit = SubmitField('Submit')
     '''index 的route处理这个表单然后把以前发布的博客列表传给模板'''
