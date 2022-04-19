c556b3e975d73ee4a2d8463c45e094d8a8cf6756
Melbourne <jiahe.zhang@ucdconnect.ie>
2022-04-19 17:41:15 +0800

    2022.4.19ZJH time:02

--- app/main/routes.py
+++ app/main/routes.py
@@ -208,7 +208,7 @@ def post(id):
     if page == -1:
         page = (post.comments.count() - 1) // \
             current_app.config['FLASKY_COMMENTS_PER_PAGE'] + 1
-    pagination = post.comments.order_by(Comment.timestamp.asc()).paginate(
+    pagination = post.comments.order_by(Comment.timestamp.desc()).paginate(
         page, per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'],
         error_out=False)
     comments = pagination.items
