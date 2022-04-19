2d5dc20859673e201759f25c8ebd1b612f7c9f00
Melbourne <jiahe.zhang@ucdconnect.ie>
2022-04-19 10:30:53 +0800

    2022.4.19ZJH time:01

--- app/main/routes.py
+++ app/main/routes.py
@@ -90,46 +90,46 @@ def edit_profile():
 @login_required
 @permission_required(Permission.FOLLOW)
 def follow(username):
-    user = User.query.filter_by(username = username).first()
+    user = User.query.filter_by(username=username).first()
     if user is None:
         flash('Invalid user. ')
         return redirect(url_for('main.index'))
     if current_user.is_following(user):
         flash('You are already following this user. ')
-        return redirect(url_for('.user', username = username))
+        return redirect(url_for('.user', username=username))
     current_user.follow(user)
     db.session.commit()
     flash('You are now following %s.' % username)
-    return redirect(url_for('.user', username = username))
+    return redirect(url_for('.user', username=username))
 
 @main.route('/followers/<username>')
 def followers(username):
-    user = User.query.filter_by(username = username).first()
+    user = User.query.filter_by(username=username).first()
     if user is None:
         flash('Invalid user. ')
         return redirect(url_for(' .index'))
-    page = request.args.get('page', 1, type = int)
+    page = request.args.get('page', 1, type=int)
     pagination = user.followers.paginate(
-        page, per_page = current_app.config['FLASKY_FOLLOWERS_PER_PAGE'],
-        error_out= False)
-    follows = [{'user': item.follower, 'timestamp':item.timestamp}
+        page, per_page=current_app.config['FLASKY_FOLLOWERS_PER_PAGE'],
+        error_out=False)
+    follows = [{'user': item.follower, 'timestamp': item.timestamp}
                for item in pagination.items]
-    return render_template('followers.html', user = user, title = "Followers of",
-                           endpoint = '.followers', pagination = pagination,
-                           follows = follows)
+    return render_template('followers.html', user=user, title="Followers of",
+                           endpoint='.followers', pagination=pagination,
+                           follows=follows)
 
 @main.route('/all')
 @login_required
 def show_all():
     resp = make_response(redirect(url_for('.index')))
-    resp.set_cookie('show_followed', '', max_age = 30*24*60*60)
+    resp.set_cookie('show_followed', '', max_age=30 * 24 * 60 * 60)
     return resp
 
 @main.route('/followed')
 @login_required
 def show_required():
     resp = make_response(redirect(url_for('.index')))
-    resp.set_cookies('show_followed', '1', max_age = 30*24*60*60)
+    resp.set_cookies('show_followed', '1', max_age=30 * 24 * 60 * 60)
     return resp
 
 @main.route('/post/<int:id>', methods=['GET', 'Post'])
@@ -147,7 +147,7 @@ def post(id):
     page = request.args.get('page', 1, type=int)
     if page == -1:
         page = (post.comments.count() - 1) // \
-            current_app.config['FLASKY_COMMENTS_PER_PAGE'] + 1
+               current_app.config['FLASKY_COMMENTS_PER_PAGE'] + 1
     pagination = post.comments.order_by(Comment.timestamp.asc()).paginate(
         page, per_page=current_app.config['FLASKY_COMMENTS_PER_PAGE'],
         error_out=False)
@@ -198,5 +198,3 @@ def change_avatar():
         flash('Your avatar has been updated.')
         return redirect(url_for('.user', username=current_user.username))
     return render_template('change_avatar.html', form=form)
-
-
