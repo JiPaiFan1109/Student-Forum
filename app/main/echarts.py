bac9a788be1f8e215e64331fa7ebd98f5979d14f
Li Luyi <luyi.li@ucdconnect.ie>
2022-05-15 19:48:55 +0800

    无

--- app/main/echarts.py
+++ app/main/echarts.py
@@ -29,7 +29,7 @@ def wordCloud_base() -> WordCloud:
         .add(series_name = "Category", data_pair = getWordPair(), shape = SymbolType.DIAMOND, is_draw_out_of_bound = True)
         .set_global_opts(
         title_opts=opts.TitleOpts(title="Category Heat", pos_left="center", pos_right="center", title_textstyle_opts=opts.TextStyleOpts(font_size=30)),
-        tooltip_opts=opts.TooltipOpts(is_show=True)
+        tooltip_opts=opts.TooltipOpts(is_show=True, formatter = "{b}")
                         )
             )
     return cloud
@@ -70,8 +70,8 @@ def getKeyWord():
     return cloudKeys
 
 def getKeyWordPair():
-    font = [10000, 6181, 4386, 4055, 2467, 2244, 1868, 1484, 1112, 865,
-            847, 582, 555, 550, 462, 366, 360, 282, 273, 265]
+    font = [200000, 8181, 6386, 6055, 4467, 3244, 2868, 2484, 2112, 1865,
+            1847, 1582, 1555, 1550, 1462, 1366, 1360, 1282, 1273, 1265]
     keyWord = getKeyWord()
     keyWordPair = []
     for i in range(len(keyWord)):
@@ -85,7 +85,7 @@ def keyWordCloud_base():
             .set_global_opts(
             title_opts=opts.TitleOpts(title="Key Word", pos_left="center", pos_right="center",
                                       title_textstyle_opts=opts.TextStyleOpts(font_size=30)),
-            tooltip_opts=opts.TooltipOpts(is_show=True)
+            tooltip_opts=opts.TooltipOpts(is_show=True, formatter = "{b}"),
         )
     )
     return cloud
@@ -117,7 +117,8 @@ def liquid_base(proportion) -> Liquid:
              ,shape="path://M367.855,428.202c-3.674-1.385-7.452-1.966-11.146-1.794c0.659-2.922,0.844-5.85,0.58-8.719 c-0.937-10.407-7.663-19.864-18.063-23.834c-10.697-4.043-22.298-1.168-29.902,6.403c3.015,0.026,6.074,0.594,9.035,1.728 c13.626,5.151,20.465,20.379,15.32,34.004c-1.905,5.02-5.177,9.115-9.22,12.05c-6.951,4.992-16.19,6.536-24.777,3.271 c-13.625-5.137-20.471-20.371-15.32-34.004c0.673-1.768,1.523-3.423,2.526-4.992h-0.014c0,0,0,0,0,0.014 c4.386-6.853,8.145-14.279,11.146-22.187c23.294-61.505-7.689-130.278-69.215-153.579c-61.532-23.293-130.279,7.69-153.579,69.202 c-6.371,16.785-8.679,34.097-7.426,50.901c0.026,0.554,0.079,1.121,0.132,1.688c4.973,57.107,41.767,109.148,98.945,130.793 c58.162,22.008,121.303,6.529,162.839-34.465c7.103-6.893,17.826-9.444,27.679-5.719c11.858,4.491,18.565,16.6,16.719,28.643 c4.438-3.126,8.033-7.564,10.117-13.045C389.751,449.992,382.411,433.709,367.855,428.202z"
              ,label_opts=opts.LabelOpts(formatter="{} OnLine".format(getOnlinePopulation()), font_size = 30, position = [80,100])
              )
-        .set_global_opts(title_opts=opts.TitleOpts(title="OnLine Population", pos_left="center", pos_right="center", pos_top="40px"))
+        .set_global_opts(title_opts=opts.TitleOpts(title="OnLine Population", pos_left="center", pos_right="center", pos_top="40px"),
+                         tooltip_opts=opts.TooltipOpts(formatter = "OnLine Population Ratio"))
     )
     return liquidBall
 
@@ -156,12 +157,11 @@ def bar3D_base() -> Bar3D:
     bar3D = (
         Bar3D(init_opts=opts.InitOpts(width="1600px", height="800px"))
             .add(
-            series_name="",
+            series_name="Category Information",
             data=get3D_points(),
             xaxis3d_opts=opts.Axis3DOpts(type_="category", data=getAllCategories(), name = "Category", interval = 0),
             yaxis3d_opts=opts.Axis3DOpts(type_="value", name = "PostAmount"),
             zaxis3d_opts=opts.Axis3DOpts(type_="value", name = "Heat"),
-
         )
             .set_global_opts(
             visualmap_opts=opts.VisualMapOpts(
@@ -179,8 +179,7 @@ def bar3D_base() -> Bar3D:
                     "#d73027",
                     "#a50026",
                 ]
-            )
-            # ,tooltip_opts=opts.TooltipOpts(formatter=JsCode())
+            ),
         )
     )
     return bar3D
